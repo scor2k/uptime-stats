46b62fe15d4e7d531290c226e0ed7689829d3e74
Alexander Konyukov <scor2k@gmail.com>
2021-11-18 22:33:46 -0500

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/scor2k/uptimestats
+
+go 1.15
+
+require (
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+	github.com/urfave/cli/v2 v2.3.0
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import "github.com/op/go-logging"
+import "github.com/urfave/cli/v2"
+import "os"
+
+var log = logging.MustGetLogger("consul-acl")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} ▶ %{level:.8s} %{shortfunc} %{color:reset} %{message}`,
+)
+
+func main() {
+	appLogs := logging.NewLogBackend(os.Stderr, "", 0)
+	appLogsFormatter := logging.NewBackendFormatter(appLogs, format)
+	appLogsLeveled := logging.AddModuleLevel(appLogsFormatter)
+	appLogsLeveled.SetLevel(logging.INFO, "")
+	logging.SetBackend(appLogsLeveled)
+
+	app := &cli.App{
+		Name:     "uptimestats",
+		HelpName: "uptimstats",
+		Usage:    "check service availability and generate reports",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "addr",
+				Value: "127.0.0.1",
+				Usage: "addr for monitoring",
+			},
+			&cli.BoolFlag{
+				Name:  "debug",
+				Value: false,
+				Usage: "Enable debug logging",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "monitor",
+				Usage: "monitor host uptime",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "addr",
+						Value:    "127.0.0.1",
+						Usage:    "addr for monitoring",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if c.Bool("debug") {
+						appLogsLeveled.SetLevel(logging.DEBUG, "")
+					}
+					log.Debug("addr: ", c.String("addr"))
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
